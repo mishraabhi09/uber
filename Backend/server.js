ec8c471faadddc71d32f3ad3aa0c5e3697855746
mishraabhi09 <abhiimishra1502@gmail.com>
2025-04-12 19:43:25 +0530

    Server Ready

---
+++ Backend/server.js
@@ -0,0 +1,35 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3000;
+
+
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+// Handle server errors
+server.on('error', (error) => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(`Port ${port} requires elevated privileges`);
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(`Port ${port} is already in use`);
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+});
